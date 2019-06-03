2364d2f22a16fe834e6251200f5fabd788f7f469
alex23ptr <alex23ptr@gmail.com>
2019-06-03 12:13:24 +0300

    added new comment

--- app/code/local/AP/ModBanners/Model/Banner.php
+++ app/code/local/AP/ModBanners/Model/Banner.php
@@ -2,7 +2,7 @@
 class AP_ModBanners_Model_Banner
     extends Mage_Core_Model_Abstract
 {
-       
+    //init the modul   
     protected function _construct()
     {
         parent::_construct();
