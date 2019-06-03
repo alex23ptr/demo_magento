cd115edac0980cf8a98d4f336d9f5990329c2ed3
alex23ptr <alex23ptr@gmail.com>
2019-06-03 13:56:34 +0300

    rest

--- app/code/local/AP/ModBanners/Model/Banner.php
+++ app/code/local/AP/ModBanners/Model/Banner.php
@@ -2,7 +2,7 @@
 class AP_ModBanners_Model_Banner
     extends Mage_Core_Model_Abstract
 {
-    //init the module   
+    //init the module 2   
     protected function _construct()
     {
         parent::_construct();
