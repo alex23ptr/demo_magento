808e8fc8ab4a14d3c14ab3f84ae420902b2d0605
alex23ptr <alex23ptr@gmail.com>
2019-06-12 14:12:52 +0300

    backup model file

---
+++ app/code/local/AP/ModBanners/Model/Banner - Copy.php
@@ -0,0 +1,12 @@
+<?php
+class AP_ModBanners_Model_Banner
+    extends Mage_Core_Model_Abstract
+{
+    //init the module 2   
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->_init('ap_modbanners/banner');
+    }  
+	 
+}
